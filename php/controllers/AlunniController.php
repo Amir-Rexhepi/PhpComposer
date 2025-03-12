211e30ad615cc3b39f27390b9c9459df8071bd7d
lai2-24 <lai2-24@itismeucci.com>
2025-03-12 10:39:17 +0100

    primo commit

--- php/controllers/AlunniController.php
+++ php/controllers/AlunniController.php
@@ -12,4 +12,41 @@ class AlunniController
     $response->getBody()->write(json_encode($results));
     return $response->withHeader("Content-type", "application/json")->withStatus(200);
   }
+
+  public function view(Request $request, Response $response, $args){
+    $mysqli_connection = new MySQLi('my_mariadb', 'root', 'ciccio', 'scuola');
+    $id = $args['id'];
+    $result = $mysqli_connection->query("SELECT * FROM alunni WHERE id = '$id'");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function create(Request $request, Response $response, $args){
+    $con = new MySQli('my_mariadb', 'root', 'ciccio', 'scuola');
+    $body = json_decode($request->getBody()->getContents(), true);
+    $nome = $body["nome"];
+    $cognome = $body["cognome"];
+    $raw_query = "INSERT INTO alunni(nome, cognome) VALUES('$nome', '$cognome')";
+    $result = $con->query($raw_query);
+    if ($result && $con->affected_rows > 0) {
+       $response->getBody()->write(json_endcode(array("message" => "Success")));
+    } else {
+      $response->getBody()->write(json_encode(array("message" => $con->error)));
+    }
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+  }
+
+  public function delete(Request $request, Response $response, $args){
+    $conn = new MySQLi('my_mariadb', 'root', 'ciccio', '');
+    $id = $args['id'];
+    $result = $conn->query("DELETE * FROM alunni WHERE id = '$id'");
+    $results = $result->fetch_all(MYSQLI_ASSOC);
+
+    $response->getBody()->write(json_encode($results));
+    return $response->withHeader("Content-type", "application/json")->withStatus(200);
+
+  }
+  
 }
--- php/index.php
+++ php/index.php
@@ -2,10 +2,19 @@
 use Slim\Factory\AppFactory;
 
 require __DIR__ . '/vendor/autoload.php';
+require __DIR__ . '/controllers/HelloController.php';
 require __DIR__ . '/controllers/AlunniController.php';
-
 $app = AppFactory::create();
 
-$app->get('/alunni', "AlunniController:index");
+$app->get('/hello',"HelloController:hello");
+$app->get('/hello/{name}',"HelloController:hello_with_name");
+$app->get('/json/{name}', "HelloController:json");
 
+$app->get('/alunni',"AlunniController:index");
+$app->get('/alunni/{id}',"AlunniController:view");
+$app->post('/alunni', "AlunniController:create");
+$app->delete('/alunni/{id}', "AlunniController:delete");
 $app->run();
+
+// curl -X POST localhost:8080/alunni --data '{"nome":"Amir","cognome":"Rexhepi"}' -H "Content-Type, application/json"
+// curl -X DELETE localhost:8080/alunni/5
